8042522b9bc052811085cfd53dfd3da295451993
Zephyrkina <alma001@ukr.net>
2018-10-06 13:31:27 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,109 +1,57 @@
 window.onload = function () {
+
+ascSortDirectionFlagForName = {value: true};
+ascSortDirectionFlagForWeight = {value: true};
+ascSortDirectionFlagForDate = {value: true};
+
 document.getElementById("sort-by-name").onclick = function() {
-	sortByName();
-};
-
-sorted_name_strict = {value: true};
-sorted_size_strict = {value: true};
-sorted_date_strict = {value: true};
-
-function sortByName() {
-	let obj = new Object();
-	let arr = new Array();
+	var ascSortingConditionByName = (a, b) =>( (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0));
+	var descSortingConditionByName = (a, b) =>( (a.name < b.name) ? 1 : ((a.name > b.name) ? -1 : 0));
 	
-	let table = document.getElementById("my-table");
-	let rows = table.rows;
-
-	parseFromTable(rows, arr);
-
-	sortRecords(arr, (a, b) =>( (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0)),
-			 (a, b) =>( (a.name < b.name) ? 1 : ((a.name > b.name) ? -1 : 0)), 
-	 		 sorted_name_strict );
-
-	writeToTable(rows, arr);
-}
-
+	sort(ascSortingConditionByName, descSortingConditionByName, ascSortDirectionFlagForName);
+};
 
 document.getElementById("sort-by-size").onclick = function() {
-	sortBySize();
+	var ascSortingConditionByWeight = (a, b) =>( (a.weight > b.weight) ? 1 : ((a.weight < b.weight) ? -1 : 0));
+	var descSortingConditionByWeight = (a, b) =>( (a.weight < b.weight) ? 1 : ((a.weight > b.weight) ? -1 : 0));
+
+	sort(ascSortingConditionByWeight, descSortingConditionByWeight, ascSortDirectionFlagForWeight);
 };
-
-
-
-function sortBySize() {
-	var obj = new Object();
-	var arr = new Array();
-	
-	table = document.getElementById("my-table");
-
-	rows = table.rows;
-
-	parseFromTable(rows, arr);
-
-	sortRecords(arr, (a, b) =>( (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0)),
-			 (a, b) =>( (a.name < b.name) ? 1 : ((a.name > b.name) ? -1 : 0)), 
-	 		 sorted_name_strict );
-
-
-	if(sorted_size_strict) {
-		arr.sort((a, b) =>( (a.weight > b.weight) ? 1 : ((a.weight < b.weight) ? -1 : 0)));
-		sorted_size_strict = !sorted_size_strict;
-	}
-	else {
-		arr.sort((a, b) =>( (a.weight < b.weight) ? 1 : ((a.weight > b.weight) ? -1 : 0)));
-		sorted_size_strict = !sorted_size_strict;
-	}
-
-	writeToTable(rows, arr);
-
-};
-
 
 document.getElementById("sort-by-date").onclick = function() {
-	sortByDate();
+	var ascSortingConditionByDate = (a, b) =>( (a.date > b.date) ? 1 : ((a.date < b.date) ? -1 : 0));
+	var descSortingConditionByDate = (a, b) =>( (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0));
+
+	sort(ascSortingConditionByDate, descSortingConditionByDate, ascSortDirectionFlagForDate);
 };
 
+function sort(ascSortingCondition, descSortingCondition, sortDirectionFlag) {
+	var table = document.getElementById("my-table");
+	var records = parseFromTable(table);
 
-function sortByDate() {
-	var obj = new Object();
-	var arr = new Array();
-	
-	table = document.getElementById("my-table");
+	records.sort(sortDirectionFlag.value ? ascSortingCondition : descSortingCondition);
+	sortDirectionFlag.value = !sortDirectionFlag.value;
 
-	rows = table.rows;
-
-	parseFromTable(rows, arr);
-
-	if(sorted_date_strict) {
-		arr.sort((a, b) =>( (a.date > b.date) ? 1 : ((a.date < b.date) ? -1 : 0)));
-		sorted_date_strict = !sorted_date_strict;
-	}
-	else {
-		arr.sort((a, b) =>( (a.date < b.date) ? 1 : ((a.date > b.date) ? -1 : 0)));
-		sorted_date_strict = !sorted_date_strict;
-	}
-
-	writeToTable(rows, arr);
-
+	writeToTable(table, records);
 };
 
-function sortRecords(arr, lambda1, lambda2, flag) {
-	arr.sort(flag.value ? lambda1 : lambda2);
-	flag.value = !flag.value;
-}
+function parseFromTable(table){
+	var rows = table.rows;
+	var records = new Array();
 
-function parseFromTable(rows, arr){
-	for (i = 1; i < rows.length; i++) {
+	for (let i = 1; i < rows.length; i++) {
 		let n = rows[i].getElementsByTagName("TD")[0].getElementsByTagName("a")[0].innerHTML.toLowerCase(); 
 		let s = rows[i].getElementsByTagName("TD")[1].innerHTML.toLowerCase();
 		let d= rows[i].getElementsByTagName("TD")[2].innerHTML.toLowerCase();
 		let record = {name:n, weight:s, date: d};
-		arr.push(record);
+		records.push(record);
 	}
+	return records;
 };
 
-function writeToTable(rows, arr){
-	for (i = 1; i < rows.length; i++) {
+function writeToTable(table, arr){
+	var rows = table.rows;
+	for (let i = 1; i < rows.length; i++) {
 		rows[i].getElementsByTagName("TD")[0].getElementsByTagName("a")[0].innerHTML = arr[i-1].name; 
 		rows[i].getElementsByTagName("TD")[1].innerHTML = arr[i-1].weight;
 		rows[i].getElementsByTagName("TD")[2].innerHTML = arr[i-1].date;
