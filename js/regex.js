86293273cfeff8a3f6d0861b24497fa6f783eff1
Valeriia Voinalovych <Valeriia_Voinalovych@epam.com>
2018-10-11 17:07:28 +0300

    added vlidation

--- js/regex.js
+++ js/regex.js
@@ -1,32 +1,86 @@
 window.onload = function () {
-
+/*
 	var elem = document.getElementById("text");
-	console.log(elem);
-	var regex = "^[0-9a-zA-Zа-яёА-ЯЁ_.-]{4,20}\@[a-zA-Zа-яёА-ЯЁ0-9]{2,5}(\.[a-zA-Zа-яёА-ЯЁ0-9]{2,5}){1,2}$";
-	var login = document.getElementById("inputLogin");
+	console.log(elem);*/
+	//var regex = "^[0-9a-zA-Zа-яёА-ЯЁ_.-]{4,20}\@[a-zA-Zа-яёА-ЯЁ0-9]{2,5}(\.[a-zA-Zа-яёА-ЯЁ0-9]{2,5}){1,2}$";
 	
+	//regex = "^([^\.](?:.[a-zA-Zа-яёА-ЯЁ]+)[0-9a-zA-Zа-яёА-ЯЁ_.-]{4,20})\@[a-zA-Zа-яёА-ЯЁ0-9]{2,5}(\.[a-zA-Zа-яёА-ЯЁ0-9]{2,5}){1,2}";
+ 	login = document.getElementById("inputLogin");
+	password = document.getElementById("inputPassword");
+
+    UpLowRegex="^((?=.*[a-z])(?=.*[A-Z])).{8,20}$";
+     charReg="^(?=.*[-+_!@#$%^&*.,?]).{8,20}$";
+
+	
+	//^([^\.](?:.[a-zA-Zа-яёА-ЯЁ]+)[0-9a-zA-Zа-яёА-ЯЁ_.-]{4,20})\@[a-zA-Zа-яёА-ЯЁ0-9]{2,5}(\.[a-zA-Zа-яёА-ЯЁ0-9]{2,5}){1,2}$
 
 
-	var handler = function(){
-		
-		if ((login.value).match(regex)){
-			alert("matches");
-			console.log("matches");
+	/*var handler = function(){
+		var pass = password.value;
+		var log = login.value;
+		if (!((log).match(regex))){
+			alert("login not matches");
+		} else {
+			console.log(log + ".");
+			log.trim();
+			console.log(log + ".");
+
+
 		}
-		else {
-			console.log("not matches");
-			alert("not matches");
+		*/
+/*
+		var message;
+
+		if( pass.length < 8){
+			message = "password is too short";
+			//alert("password is too short");
+		} else
+		if( pass.length > 20) {
+			message = "password is too long";
+			//alert("password is too long");
+		} else
+		if(!pass.match(UpLowRegex)){
+			message ="must contain at least one lower case char and one uppaer case char";
+			//alert("must contain at least one lower case char and one uppaer case char");
+		} else
+		if(!pass.match(charReg)){
+			message ="must contain 3-5 special symbols(ex:-+_!@#$%^&*.,?)";
+
+			//alert("must contain 3-5 special symbols(ex:-+_!@#$%^&*.,?)");
 		}
-	};
+		password.setCustomValidity(message);*/
 
-/*var handler = function() {
-	console.log(login.value);
-}*/
+/*	};*/
 
-	elem.addEventListener("click", handler);
+
+	//elem.addEventListener("submit", handler);
+	password.addEventListener("blur", function(e){
+		console.log("afsas");
+		var message ="";
+		var pass = password.value;
+
+		if( pass.length < 8){
+			message += "password is too short";
+			//alert("password is too short");
+		} else
+		if( pass.length > 20) {
+			message += "password is too long";
+		} else
+		if(!pass.match(UpLowRegex)){
+			message +="must contain at least one lower case char and one uppaer case char";
+		} else
+		if(!pass.match(charReg)){
+			message +="must contain 3-5 special symbols(ex:-+_!@#$%^&*.,?)";
+
+			//alert("must contain 3-5 special symbols(ex:-+_!@#$%^&*.,?)");
+		}
+		password.setCustomValidity(message);
+	});
 
 };
 
 /*
 ^[0-9a-zA-Zа-яёА-ЯЁ_.-]{4,20}\@[a-zA-Zа-яёА-ЯЁ0-9]{2,5}(\.[a-zA-Zа-яёА-ЯЁ0-9]{2,5}){1,2}$ 
+
+^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])).*$
 */
